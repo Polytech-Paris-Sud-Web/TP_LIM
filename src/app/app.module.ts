dc5f78988c46ab44e5687c2a3127660bea65f769
TonyLim <tony-lim@hotmail.fr>
2021-03-15 17:17:25 +0100

    router done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {HttpClientModule} from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,6 +10,13 @@ import { ArticleComponent } from './article/article.component';
 import { ArticlesComponent } from './articles/articles.component';
 import { ArticleService } from "./article.service";
 import { ArticleCreationComponent } from './article-creation/article-creation.component';
+
+const appRoutes: Routes = [
+  { path: 'create', component: ArticleCreationComponent },
+  { path: 'articles', component: ArticlesComponent },
+  { path: '', component: ArticlesComponent }
+]
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -17,6 +25,10 @@ import { ArticleCreationComponent } from './article-creation/article-creation.co
     ArticleCreationComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
